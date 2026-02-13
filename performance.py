ebd6817dd8d43f53adb0dc7bdeedca8fedce24c0
ipatel-garc <ipatelgarcia@gmail.com>
2026-02-13 19:29:29 +0000

    Task

--- performance.py
+++ performance.py
@@ -1,25 +1,17 @@
 #performance task code
 
 score=[]
-Vocabulary= {
-    "Eschew": ["Deliberately avoid using"],
-    "Sonder": ["The profound, individual realization that everyone has a life as vivid and complex as your own."],
-    "Hubris": ["Overbearing pride or self-confidence"],
-    "Abstruse": ["Difficult to understand"],
-    "Callous": ["Disregard for others"],
-    "Ebullient": ["Full of energy, cheerful"],
-    "Disparate": ["Of a distinct kind"],
-    "Fractious": ["Inclined to make trouble; unruly"],
-    "Kinetic": ["Relating or resulting from motion"],
-    "Specious": ["Superficially plausible. but actually wrong; misleading in appearance"]
-}
+Vocabulary = {"Eschew", "Sonder", "Hubris", "Abstruse", "Callous", "Ebullient", "Disparate", "Fractious", "Kinetic", "Specious"}
+Definition = ["To deliberately avoid using", "the profound, individual realization that everyone has a life as vivid and complex as your own.", "overbearing pride or self-confidence", "difficult to understand", "disregard for others", "full of energy, cheerful", "of a distinct kind", "Inclined to make trouble; unruly", "relating or resulting from motion", "superficially plausible. but actually wrong; misleading in appearance"]
 
-def vocab_quiz():
+
+
+for define, vocab in zip(Vocabulary, Definition):
     question=input("Would you like to take a vocab quiz? Yes or No?: ")
     while question =="Yes":
-        print("Here are the vocab terms, study if needed: ", list(Vocabulary.keys()) )
-        question1 = print(input("Deliberately avoid u: sing"))
-        if question1 == "Eschew":
+            # print("Here are the vocab terms, study if needed: ", list(Vocabulary.keys()) )
+        question1 = input(f"What word means {Definition}:")
+        if question1 == word:
             print("Correct")
             score.append(1)
             print(score)
@@ -29,4 +21,4 @@ def vocab_quiz():
     
 
 
-vocab_quiz()
+vocab_quiz("Eschew", "To deliberately avoid using")
