c3b4115b917fc714915ec01a0561d85ba7b0b3fc
ipatel-garc <ipatelgarcia@gmail.com>
2026-02-25 19:22:07 +0000

    CollegeB

--- performance.py
+++ performance.py
@@ -1,6 +1,7 @@
 #performance task code
-
+import random
 score=[]
+wrong_answers=[]
 Vocabulary = {
               "Eschew": ["To deliberately avoid using"],
               "Sonder": ["the profound, individual realization that everyone has a life as vivid and complex as your own."], 
@@ -13,26 +14,34 @@ Vocabulary = {
               "Kinetic": ["relating or resulting from motion"], 
               "Specious": ["superficially plausible. but actually wrong; misleading in appearance"]
 }
+name=input("What is your name?:")
 question=input("Would you like to take a vocab quiz? Yes or No?: ")
 def vocabquiz(vocab, definition):
         while question =="Yes":
-            print("Here are the vocab terms, study if needed: ", list(Vocabulary.keys()) )
-            question1 = input(f"What word means {definition}?:")
+            print("Here are the vocab terms, study if needed: ",list(Vocabulary.keys()))            
+            question1 = input(f"What word means {definition}?:") 
             if question1 == vocab:
                 print("Correct")
                 score.append("correct")
                 return(score)
             else:
                 print("Incorrect")
-                score.append("incorrect")
-                return(score)
-    
-    
+                wrong_answers.append(vocab)
+                return(wrong_answers)
 
+    
+def scores():
+    results=input("Would you like to see your r?Yes or No:")
+    if results== "Yes":
+        result=len(score) * 10
+        print(f"You got a {result}%!")
+        print(wrong_answers)
+    elif results=="No":
+        print("Thank you for taking the test!")
 
 vocabquiz("Sonder", "the profound, individual realization that everyone has a life as vivid and complex as your own.")
 vocabquiz("Fractious", "Inclined to make trouble; unruly")
-vocabquiz("Ebullient","full of energy; cheerful")
+vocabquiz("Ebullient", "full of energy; cheerful")
 vocabquiz("Hubris","overbearing pride or self-confidence")
 vocabquiz("Kinetic","relating or resulting from motion")
 vocabquiz("Callous","disregard for others")
@@ -40,3 +49,5 @@ vocabquiz("Eschew","To deliberately avoid using")
 vocabquiz("Specious","superficially plausible. but actually wrong; misleading in appearance")
 vocabquiz("Abstruse","difficult to understand")
 vocabquiz("Disparate","of a distinct kind")
+scores()
+
